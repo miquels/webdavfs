984490ecb9d49e801e685a724b8e242a2ce2a427
Miquel van Smoorenburg <mike@langeraar.net>
2017-07-02 15:41:35 +0200

    move stuff around

--- fuse.go
+++ fuse.go
@@ -44,6 +44,58 @@ func (fs *FS) Root() (fs.Node, error) {
 	return &NodeDir{ dav: fs.dav, Path: "/" }, nil
 }
 
+func (nd *NodeDir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (ret fs.Node, err error) {
+	path := nd.Path + req.Name + "/"
+	err = nd.dav.Mkcol(path)
+	if err == nil {
+		now := time.Now()
+		rnd := &NodeDir{
+			dav: nd.dav,
+			Path: path,
+		}
+		rnd.Name = req.Name
+		rnd.Mtime = now
+		rnd.Ctime = now
+		rnd.IsDir = true
+		ret = rnd
+	}
+	return
+}
+
+func (nd *NodeDir) Rename(ctx context.Context, req *fuse.RenameRequest, newDir fs.Node) (err error) {
+	oldPath := joinPath(nd.Path, req.OldName)
+	newPath := joinPath(newDir.(*NodeDir).Path, req.NewName)
+
+	dnode, err := nd.dav.Stat(oldPath)
+	if err != nil {
+		return
+	}
+	if dnode.IsDir {
+		oldPath += "/"
+		newPath += "/"
+	}
+	err = nd.dav.Move(oldPath, newPath)
+	return
+}
+
+func (nd *NodeDir) Remove(ctx context.Context, req *fuse.RemoveRequest) (err error) {
+	path := joinPath(nd.Path, req.Name)
+	props, err := nd.dav.PropFindWithRedirect(path, 1, nil)
+	if err != nil {
+		return
+	}
+	if len(props) != 1 {
+		return fuse.Errno(syscall.ENOTEMPTY)
+	}
+	for _, p := range props {
+		if p.ResourceType == "collection" {
+			path += "/"
+		}
+	}
+	err = nd.dav.Delete(path)
+	return
+}
+
 func (nd *NodeDir) Attr(ctx context.Context, a *fuse.Attr) (err error) {
 	fmt.Printf("nodedir.Attr %s\n", nd.Path)
 	a.Inode = 0 // getInodeNum(nd.Path, true)
@@ -88,6 +140,46 @@ func (nd *NodeDir) ReadDirAll(ctx context.Context) (dd []fuse.Dirent, err error)
 	return
 }
 
+func (nd *NodeDir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (node fs.Node, handle fs.Handle, err error) {
+	trunc := flagSet(req.Flags, fuse.OpenTruncate)
+	creat := flagSet(req.Flags, fuse.OpenCreate)
+	read  := req.Flags.IsReadWrite() || req.Flags.IsReadOnly()
+	write := req.Flags.IsReadWrite() || req.Flags.IsWriteOnly()
+	excl  := flagSet(req.Flags, fuse.OpenExclusive)
+	fmt.Printf("Create %s: trunc %v create %v read %v write %v excl %v\n", req.Name, trunc, creat, read, write, excl)
+	path := joinPath(nd.Path, req.Name)
+	created := false
+	if trunc {
+		created, err = nd.dav.Put(path, []byte{})
+	} else {
+		created, err = nd.dav.PutRange(path, []byte{}, 0)
+	}
+	if err != nil {
+		return
+	}
+	if excl && !created {
+		err = fuse.EEXIST
+		return
+	}
+	dnode, err := nd.dav.Stat(path)
+	if err != nil {
+		return
+	}
+	n := &NodeFile{
+		Dnode: dnode,
+		Path: path,
+		dav: nd.dav,
+	}
+	node = n
+	handle = n
+	return
+}
+
+
+func (nd *NodeDir) Release(ctx context.Context, req *fuse.ReleaseRequest) error {
+	return nil
+}
+
 func (nf *NodeFile) Attr(ctx context.Context, a *fuse.Attr) (err error) {
 	fmt.Printf("nodefile.Attr %s\n", nf.Path)
 	a.Inode = 0 // getInodeNum(nf.Path, false)
@@ -95,6 +187,55 @@ func (nf *NodeFile) Attr(ctx context.Context, a *fuse.Attr) (err error) {
 	a.Size = nf.Size
 	a.Ctime = nf.Ctime
 	a.Mtime = nf.Mtime
+	return
+}
+
+func (nf *NodeFile) Setattr(ctx context.Context, req *fuse.SetattrRequest, resp *fuse.SetattrResponse) (err error) {
+	v := req.Valid
+	if attrSet(v, fuse.SetattrMode) ||
+	   attrSet(v, fuse.SetattrUid) ||
+	   attrSet(v, fuse.SetattrGid) {
+		return fuse.EPERM
+	}
+	if attrSet(v, fuse.SetattrSize) {
+		fmt.Printf("Setattr %s %d\n", nf.Name, req.Size)
+		if req.Size == 0 {
+			if nf.Size > 0 {
+				_, err = nf.dav.Put(nf.Path, []byte{})
+				if err != nil {
+					return
+				}
+			}
+		} else if req.Size >= nf.Size {
+			_, err = nf.dav.PutRange(nf.Path, []byte{}, int64(req.Size))
+			if err != nil {
+				return
+			}
+		} else {
+			err = fuse.ERANGE
+			return
+		}
+		nf.Size = req.Size
+	}
+	// if attrSet(v, fuse.SetattrAtime) {
+	// 	nf.Atime = req.Atime
+	// }
+	if attrSet(v, fuse.SetattrMtime){
+		nf.Mtime = req.Mtime
+	}
+	attr := fuse.Attr{
+		Size:	nf.Size,
+		Blocks:	nf.Size / 512,
+		// Atime: nf.Atime,
+		Mtime: nf.Mtime,
+		Ctime: nf.Ctime,
+		Mode: 0644,
+		Nlink: 1,
+		Uid: 0,
+		Gid: 0,
+		BlockSize: 4096,
+	}
+	resp.Attr = attr
 	return
 }
 
@@ -154,139 +295,7 @@ func (nf *NodeFile) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.
 	return nf, nil
 }
 
-func (nf *NodeFile) Setattr(ctx context.Context, req *fuse.SetattrRequest, resp *fuse.SetattrResponse) (err error) {
-	v := req.Valid
-	if attrSet(v, fuse.SetattrMode) ||
-	   attrSet(v, fuse.SetattrUid) ||
-	   attrSet(v, fuse.SetattrGid) {
-		return fuse.EPERM
-	}
-	if attrSet(v, fuse.SetattrSize) {
-		fmt.Printf("Setattr %s %d\n", nf.Name, req.Size)
-		if req.Size == 0 {
-			if nf.Size > 0 {
-				_, err = nf.dav.Put(nf.Path, []byte{})
-				if err != nil {
-					return
-				}
-			}
-		} else if req.Size >= nf.Size {
-			_, err = nf.dav.PutRange(nf.Path, []byte{}, int64(req.Size))
-			if err != nil {
-				return
-			}
-		} else {
-			err = fuse.ERANGE
-			return
-		}
-		nf.Size = req.Size
-	}
-	// if attrSet(v, fuse.SetattrAtime) {
-	// 	nf.Atime = req.Atime
-	// }
-	if attrSet(v, fuse.SetattrMtime){
-		nf.Mtime = req.Mtime
-	}
-	attr := fuse.Attr{
-		Size:	nf.Size,
-		Blocks:	nf.Size / 512,
-		// Atime: nf.Atime,
-		Mtime: nf.Mtime,
-		Ctime: nf.Ctime,
-		Mode: 0644,
-		Nlink: 1,
-		Uid: 0,
-		Gid: 0,
-		BlockSize: 4096,
-	}
-	resp.Attr = attr
-	return
-}
-
-func (nd *NodeDir) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (node fs.Node, handle fs.Handle, err error) {
-	trunc := flagSet(req.Flags, fuse.OpenTruncate)
-	creat := flagSet(req.Flags, fuse.OpenCreate)
-	read  := req.Flags.IsReadWrite() || req.Flags.IsReadOnly()
-	write := req.Flags.IsReadWrite() || req.Flags.IsWriteOnly()
-	excl  := flagSet(req.Flags, fuse.OpenExclusive)
-	fmt.Printf("Create %s: trunc %v create %v read %v write %v excl %v\n", req.Name, trunc, creat, read, write, excl)
-	path := joinPath(nd.Path, req.Name)
-	created := false
-	if trunc {
-		created, err = nd.dav.Put(path, []byte{})
-	} else {
-		created, err = nd.dav.PutRange(path, []byte{}, 0)
-	}
-	if err != nil {
-		return
-	}
-	if excl && !created {
-		err = fuse.EEXIST
-		return
-	}
-	dnode, err := nd.dav.Stat(path)
-	if err != nil {
-		return
-	}
-	n := &NodeFile{
-		Dnode: dnode,
-		Path: path,
-		dav: nd.dav,
-	}
-	node = n
-	handle = n
-	return
-}
-
-func (nd *NodeDir) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (ret fs.Node, err error) {
-	path := nd.Path + req.Name + "/"
-	err = nd.dav.Mkcol(path)
-	if err == nil {
-		now := time.Now()
-		rnd := &NodeDir{
-			dav: nd.dav,
-			Path: path,
-		}
-		rnd.Name = req.Name
-		rnd.Mtime = now
-		rnd.Ctime = now
-		rnd.IsDir = true
-		ret = rnd
-	}
-	return
-}
-
-func (nd *NodeDir) Rename(ctx context.Context, req *fuse.RenameRequest, newDir fs.Node) (err error) {
-	oldPath := joinPath(nd.Path, req.OldName)
-	newPath := joinPath(newDir.(*NodeDir).Path, req.NewName)
-
-	dnode, err := nd.dav.Stat(oldPath)
-	if err != nil {
-		return
-	}
-	if dnode.IsDir {
-		oldPath += "/"
-		newPath += "/"
-	}
-	err = nd.dav.Move(oldPath, newPath)
-	return
-}
-
-func (nd *NodeDir) Remove(ctx context.Context, req *fuse.RemoveRequest) (err error) {
-	path := joinPath(nd.Path, req.Name)
-	props, err := nd.dav.PropFindWithRedirect(path, 1, nil)
-	if err != nil {
-		return
-	}
-	if len(props) != 1 {
-		return fuse.Errno(syscall.ENOTEMPTY)
-	}
-	for _, p := range props {
-		if p.ResourceType == "collection" {
-			path += "/"
-		}
-	}
-	err = nd.dav.Delete(path)
-	return
+func (nf *NodeFile) Release(ctx context.Context, req *fuse.ReleaseRequest) error {
+	return nil
 }
 
